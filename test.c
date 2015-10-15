b7ea421e798822cf8e396bffd4a53db48047f548
Tatsuya Takeshita <aquari1230@gmail.com>
2015-10-15 09:38:34 +0900

    機能の追加

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #define I 30
 #define J 10
 
@@ -6,17 +7,20 @@ int func(int a, int b);
 
 int main(void)
 {
-  int i, j, ans = 0;
+  int i, j, ans1 = 0;
+  double ans2 = 0;
 
   for(i = 0 ; i <= I ; i++)
   {
     for(j = 0 ; j <= J ; j++)
     {
-      ans += i * j;
+      ans1 += i * j;
+
+      ans2 += cos(i * j);
     }
   }
 
-  printf("ans = %d\n", ans);
+  printf("ans1 = %d ans2 = %f\n", ans1, ans2);
 
   return 0;
 }
