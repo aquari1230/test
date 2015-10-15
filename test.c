53a6582222735bba55f287832a351f0773327aaf
Tatsuya Takeshita <aquari1230@gmail.com>
2015-10-15 09:28:32 +0900

    定義・関数の追加

--- test.c
+++ test.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#define I 30
+#define J 10
+
+int func(int a, int b);
 
 int main(void)
 {
   int i, j, ans = 0;
 
-  for(i = 0 ; i <= 30 ; i++)
+  for(i = 0 ; i <= I ; i++)
   {
-    for(j = 0 ; j <= 10)
+    for(j = 0 ; j <= J ; j++)
     {
       ans += i * j;
     }
@@ -15,4 +19,9 @@ int main(void)
   printf("ans = %d\n", ans);
 
   return 0;
+}
+
+int func(int a, int b)
+{
+  return i * j;
 }
